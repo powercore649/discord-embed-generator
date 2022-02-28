7ef6e101f07ea30eb9f943c75eda8738b5a3f581
Julian <klinger2709@gmail.com>
2022-02-28 14:34:04 +0100

    init

---
+++ index.js
@@ -0,0 +1,160 @@
+new Vue({
+    el: '#app',
+    data: {
+        message: null,
+        cbxColor: false,
+        embed: {
+            color: "",
+            title: 'This is the title',
+            url: 'https://discord.js.org',
+            author: {
+                name: 'Kater-Bot',
+                icon_url: 'https://cdn.discordapp.com/avatars/785894890290544680/d41b507f2a397929104c647c86037a77.webp?size=80',
+                url: 'https://discord.js.org',
+            },
+            description: 'This is the description',
+            thumbnail: {
+                url: 'https://picsum.photos/200/200',
+            },
+            fields: [{
+                    name: 'Regular field Title',
+                    value: 'Some value here',
+                },
+                {
+                    name: '\u200b',
+                    value: '\u200b',
+                    inline: false,
+                },
+                {
+                    name: 'Inline field title',
+                    value: 'Some value here',
+                    inline: true,
+                },
+                {
+                    name: 'Inline field title',
+                    value: 'Some value here',
+                    inline: true,
+                },
+                {
+                    name: 'Inline field title',
+                    value: 'Some value here',
+                    inline: true,
+                },
+            ],
+            image: {
+                url: 'https://picsum.photos/500/500',
+            },
+            timestamp: new Date(),
+            footer: {
+                text: 'Some footer text here',
+                icon_url: 'https://cdn.discordapp.com/avatars/785894890290544680/d41b507f2a397929104c647c86037a77.webp?size=80',
+            },
+        },
+    },
+
+    methods: {
+        printTimestamp: function (timestamp) {
+            return timestamp.toLocaleString();
+        },
+
+        // from https://stackoverflow.com/questions/5717093/check-if-a-javascript-string-is-a-url
+        isValidURL: function (str) {
+            var res = str.match(/(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/g);
+            return (res !== null)
+        },
+
+        toEmbedStr: function () {
+            const embedToPrint = Object.assign({}, this.embed);
+
+            for (let i = 0; i < embedToPrint.fields; i++) {
+                embedToPrint.fields[i].name = embedToPrint.fields[i].name ? embedToPrint.fields[i].name : '\\u200b';
+            }
+
+            for (let key of Object.keys(embedToPrint)) {
+                let value = embedToPrint[key];
+                if (value == null || value == '') {
+                    delete embedToPrint[key];
+                }
+            }
+
+            if (embedToPrint.author.name === "" && embedToPrint.author.icon_url === "" && embedToPrint.author.url === "")
+                delete embedToPrint.author;
+
+            if (embedToPrint.thumbnail.url === "")
+                delete embedToPrint.thumbnail;
+
+            if (embedToPrint.image.url === "")
+                delete embedToPrint.image;
+
+            if (embedToPrint.footer.text === "" && embedToPrint.footer.icon_url === "")
+                delete embedToPrint.footer;
+
+                alert(`!richembed post ${JSON.stringify(embedToPrint)}`);
+            return JSON.stringify(embedToPrint);
+        },
+
+        isValidHexCode: function (hexCode) {
+            return /^#[0-9A-F]{6}$/i.test(hexCode);
+        },
+
+        updateColor: function () {
+            this.cbxColor = true;
+            if (this.isValidHexCode(this.embed.color)) {
+                document.body.style.setProperty('--background-color', this.embed.color);
+            } else {
+                document.body.style.setProperty('--background-color', "#202225");
+            }
+        },
+
+        clearColor: function () {
+            this.$nextTick(() => {
+                this.embed.color = "";
+                document.body.style.setProperty('--background-color', "#202225");
+            });
+        },
+
+        deleteField: function (index) {
+            this.embed.fields.splice(index, 1);
+        },
+
+        addField: function () {
+            this.embed.fields.push({
+                name: "\u200b",
+                value: "\u200b",
+                inline: false,
+            })
+        },
+
+        clearTimestamp: function (event) {
+            this.$nextTick(() => {
+                if (!event.target.checked) {
+                    this.embed.timestamp = "";
+                }
+            });
+        },
+
+        clearEmbed: function () {
+
+            for (let key of Object.keys(this.embed)) {
+                let value = this.embed[key];
+                console.log(key, typeof value);
+                if(typeof value == "string") {
+                    this.embed[key] = "";
+                }
+                this.embed.author.name = "";
+                this.embed.author.icon_url = "";
+                this.embed.author.url = "";
+                this.embed.thumbnail.url = "";
+                this.embed.image.url = "";
+                this.embed.footer.text = "";
+                this.embed.footer.icon_url = "";
+                this.embed.timestamp = null;
+                this.embed.fields = [];
+
+            }
+        },
+    },
+
+    created: function () {}
+
+})
